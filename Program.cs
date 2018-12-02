a94564509f1d016be61915af04b14c37d2b273b8
Abel Lopes <abellopes@gmail.com>
2018-12-02 11:34:26 -0200

    inicio

---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Reactive;
+using System.Reactive.Linq;
+
+namespace copy_backup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string saveAtual = null;
+            string copiaNovoSave = null;
+            double tempoBackup = 50;
+            
+            Console.WriteLine("======================================================================");
+            Console.WriteLine("======================================================================");
+            
+            Console.WriteLine("ATENÇÂO VOCÊ DEVE DELETAR OS BACKUP MAIS ANTIGOS MANUALMENTE!!!!!!!!!!");
+
+            Console.WriteLine("======================================================================");
+            Console.WriteLine("======================================================================");
+
+            Console.WriteLine("informe o de quanto em quanto tempo deseja realizar que deseja fazer backup ");
+            Console.WriteLine("(exemplo 10 é 10 segundos um minuto é 60, 2 min 120 e assim por diante)");
+            tempoBackup = double.Parse(Console.ReadLine());
+            while(tempoBackup < 10)
+            { 
+                Console.WriteLine("======================================================================");
+                Console.WriteLine("não aceita menos que 10 segundos, gera muitos beckups");
+                tempoBackup = double.Parse(Console.ReadLine());
+            }
+            var observable = Observable.Interval(TimeSpan.FromSeconds(tempoBackup)).Do((x) => { }) ; // log events
+
+            Console.WriteLine("======================================================================");
+            Console.WriteLine("informe o diretorio do save game que deseja fazer backup ex: c:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames");
+            saveAtual = Console.ReadLine();
+            if(saveAtual == null || saveAtual == ""){
+                saveAtual = "D:\\Program Files (x86)\\Ubisoft\\Ubisoft Game Launcher\\savegames";
+            } 
+            Console.WriteLine("======================================================================");
+            Console.WriteLine($"Diretorio infromado para copia foi {saveAtual}");
+            
+            if (saveAtual != null) {
+                Console.WriteLine("informe o diretorio para fazer backup ou apenas de enter o deretorio padrão é C:\\backup_save_ghost");
+                copiaNovoSave = Console.ReadLine();
+                if(copiaNovoSave == null || copiaNovoSave == ""){
+                   copiaNovoSave = "C:\\backup_save_ghost\\";
+                }    
+            }
+            Console.WriteLine("======================================================================");
+            Console.WriteLine($"Diretorio infromado para salvar a copia foi {copiaNovoSave}");
+            Console.WriteLine("======================================================================");
+            Console.WriteLine("Deseja realmente iniciar o backp Y/N");
+            string yn = Console.ReadLine();
+            Console.WriteLine("======================================================================");
+            if(yn.ToLower() == "y") {
+                //primeiro subscriber
+                observable.Subscribe((x) =>
+                {
+                    Console.WriteLine(".....................................");
+                    Console.WriteLine($"backup realizado as {DateTime.Now}");                    
+                    SistemaBackup.Backup(saveAtual, copiaNovoSave + "backup-"+ DateTime.Now.ToString("yyyy-MM-dd-hh-mm-ss"));
+                });
+            }else{
+                Console.WriteLine("======================================================================");
+                Console.WriteLine("Precione uma tecla para encerrar o programa");
+            }
+            Console.ReadLine();
+        }        
+    }
+}
---
+++ SistemaBackup.cs
@@ -0,0 +1,49 @@
+
+
+using System;
+using System.IO;
+
+namespace copy_backup{
+    public static class SistemaBackup{
+
+        public static void Backup(string diretorio, string diretorioBack, bool copiarSubPastas =  true) {
+            SistemaBackup.DirectoryCopy(diretorio, diretorioBack, copiarSubPastas);
+        }
+        private static void DirectoryCopy(string sourceDirName, string destDirName, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            DirectoryInfo dir = new DirectoryInfo(sourceDirName);
+
+            if (!dir.Exists)
+            {
+                throw new DirectoryNotFoundException("O diretorio informado não Existe: " + sourceDirName);
+            }
+
+            DirectoryInfo[] dirs = dir.GetDirectories();
+            // If the destination directory doesn't exist, create it.
+            if (!Directory.Exists(destDirName))
+            {
+                Directory.CreateDirectory(destDirName);
+            }
+            
+            // Get the files in the directory and copy them to the new location.
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                string temppath = Path.Combine(destDirName, file.Name);
+                file.CopyTo(temppath, false);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string temppath = Path.Combine(destDirName, subdir.Name);
+                    DirectoryCopy(subdir.FullName, temppath, copySubDirs);
+                }
+            }
+
+        }
+    }
+}
